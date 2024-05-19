9e5499fa1de5ebf1a003c194701e9b8b51bd3b8d
danblackadder <danblackadder@gmail.com>
2024-05-19 17:02:58 +0100

    Add lint etc

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import "@/styles/globals.css";
+import '@/styles/globals.css';
 
 const App = () => {
   return (
-    <div className="h-[200px] w-[200px] flex flex-col before:justify-center items-center">
-      <h1 className="font-sans text-bold text-2xl">Noobs</h1>
-      <a
-        className="underline text-sm font-sans text-blue-500"
-        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-      >
+    <div className="flex size-[200px] flex-col items-center before:justify-center">
+      <h1 className="text-bold font-sans text-2xl">Noobs</h1>
+      <a className="font-sans text-sm text-blue-500 underline" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
         Click me!
       </a>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json";
+import { crx } from '@crxjs/vite-plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
